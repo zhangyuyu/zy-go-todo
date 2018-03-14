e7abad4f314cf2746ca9ae1fbe7bd4f8b5670c9f
yuzhang <1580074674@qq.com>
2018-03-14 11:52:56 +0100

    [Yu] Update the port

--- hello.go
+++ hello.go
@@ -3,8 +3,13 @@ package main
 import (
 	"github.com/astaxie/beego"
 	_ "github.com/zhangyuyu/hello/routers"
+	"os"
 )
 
 func main() {
-	beego.Run()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "9090"
+	}
+	beego.Run(":" + port)
 }
